a501490f8efcd1687666bb0c80f9d4c66667fc02
Bartosz Janiak <bartoszjaniak@outlook.com>
2024-09-14 07:36:09 +0200

    projects as md

--- src/content/config.js
+++ src/content/config.js
@@ -1,14 +1,25 @@
 import { defineCollection, z } from "astro:content";
 
 const postCollection = defineCollection({
-	type: "content",
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		dateFormatted: z.string(),
-	}),
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    dateFormatted: z.string(),
+  }),
+});
+
+const projectCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    name: z.string(),
+    image: z.string(),
+    description: z.string(),
+    dateFormatted: z.string(),
+  }),
 });
 
 export const collections = {
-	post: postCollection,
+  post: postCollection,
+  project: projectCollection,
 };
