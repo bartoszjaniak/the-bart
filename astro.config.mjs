9cc58b00a05add0373ae2eb729433d5bcf189d47
Bartosz Janiak <bartoszjaniak@outlook.com>
2024-10-10 20:57:53 +0200

    optimiser and rocket

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  plugins: [
+    ViteImageOptimizer({
+      png: {
+        progressive: true,
+        quality: 75,
+      },
+    }),
+  ],
 });
