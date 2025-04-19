becea3b6906ad63292febdb2434c8e2ca30b5998
Bartosz Janiak <bartoszjaniak@outlook.com>
2025-04-19 20:27:08 +0200

    vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,28 +2,21 @@ import { defineConfig } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
-import netlify from "@astrojs/netlify";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://the-bart.com",  
+  site: "https://the-bart.com",
   integrations: [tailwind(), sitemap()],
   prefetch: {
     prefetchAll: true,
     defaultStrategy:"viewport"
   },
-  plugins: [
-    ViteImageOptimizer({
-      png: {
-        progressive: true,
-        quality: 75,
-      },
-      jpeg: {
-        quality: 75,
-        progressive: true,
-      },
-    }),
-  ]
+  output: "server",
+  adapter: vercel({
+    isr: true,
+    imageService: true,
+    webAnalytics: { enabled: true }
+  })
 });
